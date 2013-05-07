c6e02d570bd3177ded9529faf1ee32dca89a9c54
Manoj Dayaram <noj@moovweb.com>
2013-05-06 23:17:24 -0700

    fail fail, increasing workers increases time to do work, sadface.

--- main.go
+++ main.go
@@ -3,12 +3,38 @@ package main
 import (
 	"flag"
 	"github.com/mdayaram/cgofail/worker"
+	"sync"
+	"time"
 )
+
+var wait4me sync.WaitGroup
+
+func Provide(wch chan int, rch chan time.Duration, trials int) {
+	for i := 0; i < trials; i++ {
+		wch <- i
+		dur := <-rch
+		println(dur / 1000)
+	}
+	wait4me.Done()
+}
 
 func main() {
 	workers := flag.Int("w", 1, "Number of workers to allocate.")
+	concurrency := flag.Int("c", 10000, "Number of work providers (concurrency).")
+	trials := flag.Int("t", 10000, "Number of pieces of work each provider provides (trials).")
 	flag.Parse()
 
-	worker.What()
-	println("Number of workers: ", *workers)
+	sendWork := make(chan int, *workers)
+	recvResults := make(chan time.Duration, *workers)
+
+	for i := 0; i < *workers; i++ {
+		w := worker.New(sendWork, recvResults)
+		w.WorkIt()
+	}
+
+	for i := 0; i < *concurrency; i++ {
+		wait4me.Add(1)
+		go Provide(sendWork, recvResults, *trials)
+	}
+	wait4me.Wait()
 }
--- worker/worker.go
+++ worker/worker.go
@@ -1,5 +1,42 @@
 package worker
 
-func What() {
-	// you heard me.
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int jiggle() {
+	int i, sum = 0;
+	for(i = 0; i < 10000; i++) {
+		sum += i;
+	}
+	return i;
+}
+*/
+import "C"
+
+import (
+	"time"
+)
+
+type Worker struct {
+	recv chan int
+	send chan time.Duration
+}
+
+func New(recv chan int, send chan time.Duration) *Worker {
+	return &Worker{recv: recv, send: send}
+}
+
+func (w *Worker) WorkIt() {
+	go func() {
+		var start time.Time
+		var dur time.Duration
+		for {
+			<-w.recv
+			start = time.Now()
+			C.jiggle()
+			dur = time.Now().Sub(start)
+			w.send <- dur
+		}
+	}()
 }
